a054dc5aeef67ceb89d7af51921f1562d41584c5
Laura Brown <lauraebrown6@gmail.com>
2025-05-13 16:14:39 -0700

    Update logic for Gist

--- src/components/Gist.jsx
+++ src/components/Gist.jsx
@@ -1,53 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Octokit } from "octokit";
-
-const formatDate = (date) => {
-    const formattedDate = date.toLocaleDateString('en-GB', {
-        day: 'numeric', month: 'short', year: '2-digit'
-      }).replace(/ /g, '-');
-    return formattedDate;
-}
+import { pickColor } from "../functions/FetchFromGist";
 
 export default function Gist() {
 
-    const [colorsList, updateColorsList] = useState([]);
     const [pickedColor, updatePickedColor] = useState();
 
     useEffect(() => {
-        const getGist = async () => {
-            const octokit = new Octokit({});
-    
-            const colorsGist = await octokit.request('GET /gists/0039ef0a74940e0f9341319383906cd4', {
-                gist_id: '0039ef0a74940e0f9341319383906cd4',
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
-                }
-            });
-    
-            const colorsText = colorsGist.data.files["colors.json"].content;
-            const colorsJSON = JSON.parse(colorsText);
-            updateColorsList(colorsJSON);
+        async function fetchData(){
+            const response = await pickColor();
+            const rgb = response;
+            updatePickedColor(rgb);
         }
-        
-        getGist().catch(console.error);
-
+        fetchData();
     }, [])
 
-    useEffect(()=> {
-        const pickColor = () => {
-            if (colorsList.length < 1){
-                return;
-            }
-            const todaysDate = new Date();
-            const todaysDateFormatted = formatDate(todaysDate);
-            const todaysColorObj = colorsList.find(color => color.date == todaysDateFormatted);
-            const todaysColor = todaysColorObj.rgb;
-            updatePickedColor(todaysColor);
-        }
-        pickColor();
-    }, [colorsList]);
-
     return (
-        <div>{pickedColor}</div>
+        <p>{pickedColor}</p>
     );
 }
---
+++ src/functions/FetchFromGist.js
@@ -0,0 +1,32 @@
+import { Octokit } from "octokit";
+
+const formatDate = (date) => {
+    const formattedDate = date.toLocaleDateString('en-GB', {
+        day: 'numeric', month: 'short', year: '2-digit'
+      }).replace(/ /g, '-');
+    return formattedDate;
+}
+
+const getGist = async() => {
+    const octokit = new Octokit({});
+    const colorsGist = await octokit.request('GET /gists/0039ef0a74940e0f9341319383906cd4', {
+        gist_id: '0039ef0a74940e0f9341319383906cd4',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    const colorsText = colorsGist.data.files["colors.json"].content;
+    const colorsJSON = JSON.parse(colorsText);
+    
+    return colorsJSON;
+}
+
+export async function pickColor(){
+    const colorsList = await getGist();
+    const todaysDate = new Date();
+    const todaysDateFormatted = formatDate(todaysDate);
+    const todaysColorObj = colorsList.find(color => color.date == todaysDateFormatted);
+    const todaysColor = todaysColorObj.rgb;
+    return todaysColor;
+}
